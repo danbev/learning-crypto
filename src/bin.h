699033da8baf8e763d3b1d33e1b598c14dc8a6bd
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-03-28 14:09:58 +0200

    Remove print_binary_as_chars from bin.h

--- src/bin.h
+++ src/bin.h
@@ -1,7 +1,6 @@
 #include <stdint.h>
 
 void print_binary(uint8_t* b, int len);
-void print_binary_as_chars(uint8_t* bin, int bin_len);
 
 uint8_t* bin_to_hex(uint8_t* bin, int len);
 int bin_to_dec(uint8_t* b, int len);
