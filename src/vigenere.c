b1094806332331ae9ef63e404d01caaa4baf214a
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-04-09 13:47:37 +0200

    Fix vigenere encrypt function

--- src/vigenere.c
+++ src/vigenere.c
@@ -10,12 +10,15 @@ char* vigenere_encrypt(char* plaintext, char* key) {
   int len = strlen(plaintext);
   int key_len = strlen(key);
   char* ciphertext = (char*) malloc(len);
-  for (int i = 0, j = 0; i < len; i++) {
-    ciphertext[i] = mod((plaintext[i] - 97) + (key[j] - 97), 26) + 97; 
-    //printf("c: %c, k: %c, e:%c\n", plaintext[i], key[j], ciphertext[i]);
-    j++;
-    if (j == key_len) {
-      j = 0;
+  for (int i = 0, idx = 0, j = 0; i < len; i++) {
+    if (plaintext[i] >= 97 && plaintext[i] <= 122) {
+      ciphertext[idx] = mod((plaintext[i] - 97) + (key[j] - 97), 26) + 97;
+      //printf("c: %c, d: %d, k: %c, e:%c\n", plaintext[i], plaintext[i], key[j], ciphertext[i]);
+      j++;
+      idx++;
+      if (j == key_len) {
+        j = 0;
+      }
     }
   }
   return ciphertext;
